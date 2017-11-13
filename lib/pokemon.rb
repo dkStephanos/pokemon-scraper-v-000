56f03d0fdedf01f931a12ad6d52902cf238841ab
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-13 23:40:53 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -26,5 +26,6 @@ class Pokemon
   def self.find(id, db)
     attributes = db.execute("SELECT * FROM pokemon WHERE id =?", id).flatten
     poke = Pokemon.new(attributes[0], attributes[1], attributes[2], db)
+    binding.pry
   end
 end
